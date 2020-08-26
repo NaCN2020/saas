027ee5f9f0669e024fc92176281270490f465b2c
NaCN2020 <67950956+NaCN2020@users.noreply.github.com>
2020-08-26 15:22:12 +0800

    Update App.js

--- code/saas_apps/src/App.js
+++ code/saas_apps/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {HashRouter as Router, Route} from "react-router-dom"
 import Translate from "./pages/translate/translate";
 import Home from "./pages/home/home";
 import Markdown from "./pages/markdown/markdown";
