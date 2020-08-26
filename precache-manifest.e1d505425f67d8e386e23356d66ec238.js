8d61239d32084f7cd0664d47ff709e23188bf0c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-26 12:32:01 +0000

    Deploy NaCN2020/saas to github.com/NaCN2020/saas.git:gh-pages

---
+++ precache-manifest.e1d505425f67d8e386e23356d66ec238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d16bfce5a115e599dfff4beb251bde",
+    "url": "/saas/index.html"
+  },
+  {
+    "revision": "4db008777b031c00986c",
+    "url": "/saas/static/css/2.41da947d.chunk.css"
+  },
+  {
+    "revision": "a26870f922e77773790e",
+    "url": "/saas/static/css/main.d8e86dc8.chunk.css"
+  },
+  {
+    "revision": "4db008777b031c00986c",
+    "url": "/saas/static/js/2.5530f713.chunk.js"
+  },
+  {
+    "revision": "4fbde1ae2a4e32f59b5b4e0227d7c4e0",
+    "url": "/saas/static/js/2.5530f713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26870f922e77773790e",
+    "url": "/saas/static/js/main.3e3f20a4.chunk.js"
+  },
+  {
+    "revision": "dd6e5365ea20b90343fd",
+    "url": "/saas/static/js/runtime-main.e9b89be5.js"
+  }
+]);
